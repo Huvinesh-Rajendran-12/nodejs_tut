9f78efa364e6b573b49e6442522e94a44d4597ff
Huvinesh Rajendran <huvitom99@outlook.com>
2022-03-16 14:44:22 +0800

    modified

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-sum(1, 2);
+const tutorial = require('./tutorial.js');
+console.log(tutorial.sum(1, 2)); // 3
+console.log(tutorial.PI); // 3.14
+console.log(new tutorial.SomeMathObject()); // somemathobject
--- tutorial.js
+++ tutorial.js
@@ -1,4 +1,18 @@
 // sum of two numbers
 const sum = (a, b) => a + b;
 console.log(sum(1, 2)); // 3
-module.exports = sum;
+// PI the number of the circle
+const PI = 3.14;
+// class somemathobject
+/**
+ * @class somemathobject
+ * @console log the sum of two numbers
+ */
+class SomeMathObject {
+  // eslint-disable-next-line require-jsdoc
+  constructor() {
+    console.log('object created');
+  }
+}
+
+module.exports = {sum: sum, PI: PI, SomeMathObject: SomeMathObject};
