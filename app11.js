1605480e8dcf7fab6d940fb2cdb7a9fa06ed349e
Huvinesh Rajendran <huvitom99@outlook.com>
2022-03-17 15:25:20 +0800

    working with JOI validation

--- app11.js
+++ app11.js
@@ -4,6 +4,8 @@ const express = require('express');
 const path = require('path');
 // load body-parser module
 const bodyParser = require('body-parser');
+// load joi module
+const Joi = require('joi');
 // define app
 const app = express();
 // define port
@@ -16,13 +18,21 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 // app get method
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'static', 'index2.html'));
+  res.sendFile(path.join(__dirname, 'static', 'index3.html'));
 });
 // app post method
 app.post('/', (req, res) => {
-//  console.log(req.body);
-  // database call
-  res.json({success: true});
+  console.log(req.body);
+  const schema = Joi.object({
+    email: Joi.string().trim().email().required(),
+    password: Joi.string().min(5).max(10).required(),
+  });
+  const {error} = schema.validate(req.body);
+  if (error) {
+    res.send(error.details[0].message);
+  } else {
+    res.send('Success');
+  }
 });
 // app listen method
 app.listen(port, () => {
