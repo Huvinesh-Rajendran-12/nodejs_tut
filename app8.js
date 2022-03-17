b9241232bfcf1b4731fb0c25771557d17925660d
Huvinesh Rajendran <huvitom99@outlook.com>
2022-03-17 12:01:20 +0800

    initial commit

--- app8.js
+++ app8.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 // create http server instance
 const server = http.createServer((req, res) => {
   // readstream
-  const readStream = fs.createReadStream('./static/index.html');
+  const readStream = fs.createReadStream('./static/example.json');
   // write heading
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, {'Content-Type': 'application/json'});
   // pipe readstream to response
   readStream.pipe(res);
 }).listen(3000);
